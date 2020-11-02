38e9fbd2dc8d65f86c5b3ee7ec03b91cd54dbf3d
MinakoNG63 <73382317+MinakoNG63@users.noreply.github.com>
2020-11-02 22:36:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Hello world</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <h2>นางสาว รมิตา ศรีภูธร</h2>
+        <h2>น้ำผึ้ง</h2>
+        <h3>ท่านผู้โดยสารทุกท่านโปรดทราบขณะนี้เครื่องบินของเราที่จะบินไปปารากวัย</h3>
+        <h3>พร้อมที่จะออกเดินทางแล้วคะ และขอให้ผู้โดยสารทุกท่านวางใจในสายการบินของเราได้</h3>
+        <h3>เพราะถึงแม้ว่าเครื่องบินของเราจะเก่าแต่นักบินของเราก็ยังใหม่อยู่คะ</h3>
+        <p>My First Paragraph</p>
+        <img src="catto.jpg">
+        <img src="catta.jpg">
+        <br>
+        <a href="https://www.instagram.com/np_ramita/">This is a Link</a>
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+        </ul>
+    </body>
+</html>
