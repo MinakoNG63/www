d223c1fe2d5214a5cf336d8109a6e5b00cbc5aaf
MinakoNG63 <73382317+MinakoNG63@users.noreply.github.com>
2020-11-27 03:24:02 +0700

    Update show.php

--- show.php
+++ show.php
@@ -19,9 +19,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Hello</title>
-<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+
 <body>
+<div class='container'>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'minako.mysql.database.azure.com', 'it63070240@minako', 'Itf63070240', 'itflab', 3306);
@@ -29,26 +31,22 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM abc');
 ?>
 <table width="600" border="1">
   <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="150"> <div align="center">Delete </div></th>
-    <th width="150"> <div align="center">Edit </div></th>
+    <th> <div align="center">A</div></th>
+    <th> <div align="center">B</div></th>
+    <th> <div align="center">C</div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td><a href="delete.php?ID=<?= $Result["ID"]; ?>" class="button">Delete</a></td>
-    <td><a href="edit.php?ID=<?= $Result["ID"]; ?>" class="button">Edit</a></td>
+    <td><?php echo $Result['A'];?></div></td>
+    <td><?php echo $Result['B'];?></td>
+    <td><?php echo $Result['C'];?></td>
   </tr>
 <?php
 }
@@ -58,5 +56,7 @@ while($Result = mysqli_fetch_array($res))
 mysqli_close($conn);
 ?>
 <a href="form.html" class="btn btn-primary">เพิ่ม</a>
+
+</div>
 </body>
 </html>
